079f275fdd7a3f1c6c57894053bf5634783ac358
Denilson Torres <dtlima2020@gmail.com>
2023-06-27 22:30:18 -0300

    TeameventController

--- app/Controllers/Http/TeamEventsController.ts
+++ app/Controllers/Http/TeamEventsController.ts
@@ -1,14 +1,53 @@
 /* eslint-disable no-unreachable */
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import TeamEvent from 'App/Models/TeamEvent'
 
 export default class TeamEventsController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const teamevents = await TeamEvent.all()
+    console.log(new Date())
+    return teamevents
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    try {
+      const teamEvent = request.only(['date_init', 'date_end', 'eventId', 'teamId'])
+      console.log(teamEvent)
+      const newTe = await TeamEvent.create(teamEvent)
+      return newTe
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    try {
+      const teamEvent = await TeamEvent.findOrFail(params.id)
+      return teamEvent
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, params }: HttpContextContract) {
+    try {
+      const teamEvents = await TeamEvent.findByOrFail('id', params.id)
+      const data = request.only(['date_init', 'date_end', 'eventId', 'teamId'])
+      teamEvents.merge(data)
+      await teamEvents.save()
+      return teamEvents
+    } catch (error) {
+      return response.status(400).json({ error: 'Team Event not found' })
+    }
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ response, params }: HttpContextContract) {
+    try {
+      const teamEvent = await TeamEvent.findOrFail(params.id)
+      await teamEvent.delete()
+      return response.send('Team Event deleted')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
--- app/Models/Team.ts
+++ app/Models/Team.ts
@@ -11,9 +11,9 @@ export default class Team extends BaseModel {
   @column()
   public courseId: number
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true, serializeAs: null })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
 }
